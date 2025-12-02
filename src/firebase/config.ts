de3de964cbbca5c08f38da5de5a5ea676ec832a5
Voices of Haram <voicesofharam@gmail.com>
2025-12-02 15:28:46 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3821950900-67db9",
+  "appId": "1:343777395322:web:8c1450467efa04c88b71c3",
+  "apiKey": "AIzaSyBYDh61OJopF8rMnDjVfLQoUU-Xzl5-Nv8",
+  "authDomain": "studio-3821950900-67db9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "343777395322"
+};
