2e44ec4a10f70ffc01449ce68cd234a55c652fed
Voices of Haram <voicesofharam@gmail.com>
2025-12-05 17:21:10 +0000

    Supabase Jeweler's Org Free
    hameed234934@gmail.com
    
    main
    Production
    
    C

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,4 +3,16 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+// A little hack to make sure the client is not initialized during build time if env vars are not present
+export const supabase =
+  supabaseUrl && supabaseAnonKey
+    ? createClient(supabaseUrl, supabaseAnonKey)
+    : {
+        storage: {
+          from: () => ({
+            upload: () => Promise.resolve({ error: { message: 'Supabase not initialized' } }),
+            remove: () => Promise.resolve({ error: { message: 'Supabase not initialized' } }),
+            getPublicUrl: () => ({ data: { publicUrl: '' } }),
+          }),
+        },
+      };
